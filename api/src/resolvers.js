7f6d8125d5377e26845f4bb926ff7e94d2c82055
joshualan <joshualan23@gmail.com>
2023-08-06 19:53:59 -0400

    added everything

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,38 @@
 
 module.exports = {
   Query: {
-    
+    pets: (_, { input }, ctx) => {
+      if (input) {
+        return ctx.models.Pet.findMany({ ...input });
+      }
+
+      return ctx.models.Pet.findMany();
+    },
+    pet: (_, { id }, ctx) => {
+      return ctx.models.Pet.findOne({ id: id });
+    },
+    user: (_, __, ctx) => {
+      return ctx.models.User.findOne();
+    },
   },
   Mutation: {
-    
+    createPet: (_, { input }, ctx) => {
+      return ctx.models.Pet.create({ ...input });
+    },
   },
   Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
+    //   img(pet) {
+    //     return pet.type === "DOG"
+    //       ? "https://placedog.net/300/300"
+    //       : "http://placekitten.com/300/300";
+    //   },
+    user: (_, __, ctx) => {
+      return ctx.models.User.findOne();
+    },
   },
   User: {
-    
-  }
-}
+    pets: (user, __, ctx) => {
+      return ctx.models.Pet.findMany({ userId: user.id });
+    },
+  },
+};
--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,47 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+    pets: [Pet]
+  }
 
+  enum PetType {
+    CAT
+    DOG
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+    user: User
+  }
+
+  input PetInput {
+    name: String
+    type: String
+  }
+
+  type Query {
+    pets(input: PetInput): [Pet]!
+    pet(id: ID!): Pet
+    user: User
+  }
+
+  input CreatePetInput {
+    name: String!
+    type: String!
+  }
+
+  type Mutation {
+    createPet(input: CreatePetInput!): Pet!
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
